2bac8bcea772e683eb50e04a3b13f01bdbe4fb4a
Hossam Mourad <ihos4m@gmail.com>
2019-03-01 18:12:40 +0400

    Improve styles

--- src/components/UserWidget/UserWidget.js
+++ src/components/UserWidget/UserWidget.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function UserWidget({ user }) {
   return (
     <div className="timeline__tweet__author">
-      <span className="timeline__tweet__author__name">{user.name}</span>
+      <b className="timeline__tweet__author__name">{user.name}</b>
       <img
         src={user.avatar}
         alt={user.name}
